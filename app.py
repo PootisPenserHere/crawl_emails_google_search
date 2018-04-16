30464a11c98951b8bc55c63145257a31ff7d7a0e
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-04-15 18:37:04 -0600

    [add] Code moved to functions

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
-import mechanize
-from bs4 import BeautifulSoup
 import re
 import json
 
-# Initialize the browser
-browser = mechanize.Browser()
-browser.set_handle_robots(False)
-browser.set_handle_equiv(False)
-browser.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0')]
-browser.open('http://www.google.com.mx/')
+# Configuration variables
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'
+search_term = 'python'
+result_pages_checked = 3
 
-# Querying to google
-browser.select_form(name='f')
-browser.form['q'] = 'python'
-data = browser.submit() # Search result
+class Crawler(object):
+    def __init__(self, user_agent, search_term):
+        import mechanize
+        from bs4 import BeautifulSoup
 
-# Returned data
-links = []
+        self.mechanize = mechanize
+        self.BeautifulSoup = BeautifulSoup
 
-# Parsing the html
-soup = BeautifulSoup(data.read(),"html5lib")
-for a in soup.select('.r a'):
-    links.append(a['href'])
+        # Returned data
+        self.links = [] # Will contain all the links found
 
-# TODO: clean up the re used code
-for i in range(0, 3):
-    data = browser.follow_link(text='Next')
-    soup = BeautifulSoup(data.read(),"html5lib")
+        # Initialize the browser
+        self.browser = self.mechanize.Browser()
+        self.browser.set_handle_robots(False)
+        self.browser.set_handle_equiv(False)
+        self.browser.addheaders = [('User-agent', user_agent)]
+        self.browser.open('http://www.google.com.mx/')
 
-    for a in soup.select('.r a'):
-        links.append(a['href'])
+        # Querying to google
+        self.data = ''
+        self.browser.select_form(name='f')
+        self.browser.form['q'] = search_term
+        self.data = self.browser.submit() # Search result
 
-print json.dumps(links)
+    def parse_results(self):
+        soup = self.BeautifulSoup(self.data.read(),"html5lib")
+        for a in soup.select('.r a'):
+            self.links.append(a['href'])
+
+    def fetch_results(self, result_pages_checked):
+        # Starts by fetching the current page obtained when initializing
+        self.parse_results()
+
+        # If more than one page of results is wanted
+        if result_pages_checked > 1:
+            for i in range(1, result_pages_checked):
+                self.data = self.browser.follow_link(text='Next')
+                self.parse_results()
+
+        return self.links
+
+Crawler = Crawler(user_agent, search_term)
+print json.dumps(Crawler.fetch_results(result_pages_checked))
